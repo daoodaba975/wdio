bf35f58ecd4d93e6c2841fdfb03f1f871e06c05a
daoodaba975 <davinhoba9@outlook.fr>
2023-07-13 22:07:47 +0000

    ✅ :: add description & update.

---
+++ Readme.md
@@ -0,0 +1,15 @@
+# Full WDIO
+
+A fully structured project with Webdriver IO ✅
+
+## Install dependencies
+
+```bash
+npm install
+```
+
+## Run test script
+
+```bash
+npx wdio wdio.conf.js
+```
--- test/specs/example.e2e.js
+++ test/specs/example.e2e.js
@@ -1,20 +1,32 @@
-require("../url/herokuappUrl.js");
-require("../locateurs/herokuappLocateurs.js");
-// require("../fonctions/navigateus/LancerLeNavigateur.js");
-import { LancerLeNavigateur } from "../fonctions/navigateurs/LancerLeNavigateur.js";
+const navigateToURL = require("../fonctions/navigateurs/LancerLeNavigateur.js");
+const clickToElement = require("../fonctions/navigations/CliquerSurElement.js");
+const setValueToInput = require("../fonctions/navigations/EntrerUneValeur.js");
+const verifyExisting = require("../fonctions/verifications/VerifierSiExiste.js");
+const containingText = require("../fonctions/verifications/SiContientTexte.js");
+const toPause = require("../fonctions/navigateurs/FairePause.js");
 
-describe("My Login application", () => {
-  it("should login with valid credentials", async () => {
-    // await browser.url(heroku.appUrl);
-    await LancerLeNavigateur(heroku.appUrl);
+const { usr, pwd, vrf } = require("../variables/variables.js");
+const { theInternetURL } = require("../url/herokuappUrl.js");
+const locateurs = require("../locateurs/herokuappLocateurs.js");
 
-    await $(lock.username).setValue("tomsmith");
-    await $(lock.password).setValue("SuperSecretPassword!");
-    await $(lock.submit).click();
+describe("Mon application de connexion", () => {
+  it("doit se connecter avec des informations d'identification valides", async () => {
+    await navigateToURL(browser, theInternetURL);
+    await toPause(3);
 
-    await expect($(lock.verify)).toBeExisting();
-    await expect($(lock.verify)).toHaveTextContaining(
-      "You logged into a secure area!"
-    );
+    await setValueToInput(locateurs.herokuappLocateurs.username, usr);
+    await toPause(3);
+
+    await setValueToInput(locateurs.herokuappLocateurs.password, pwd);
+    await toPause(3);
+
+    await clickToElement(locateurs.herokuappLocateurs.submit);
+    await toPause(3);
+
+    await verifyExisting(locateurs.herokuappLocateurs.verify);
+    await toPause(3);
+
+    await containingText(locateurs.herokuappLocateurs.verify, vrf);
+    await toPause(3);
   });
 });
